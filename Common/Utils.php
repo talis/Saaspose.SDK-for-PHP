f5eae46d568f2e576012305e8234c58a8f42ebc0
Assad Mahmood Qazi <assadvirgo@gmail.com>
2013-02-20 11:50:42 +0500

    Error handeling in ProcessCommand function.

--- Common/Utils.php
+++ Common/Utils.php
@@ -28,6 +28,65 @@ class Utils {
      *
      *
      */
+	
+	public static $http_codes = array(
+			100 => 'Continue',
+			101 => 'Switching Protocols',
+			102 => 'Processing',
+			200 => 'OK',
+			201 => 'Created',
+			202 => 'Accepted',
+			203 => 'Non-Authoritative Information',
+			204 => 'No Content',
+			205 => 'Reset Content',
+			206 => 'Partial Content',
+			207 => 'Multi-Status',
+			300 => 'Multiple Choices',
+			301 => 'Moved Permanently',
+			302 => 'Found',
+			303 => 'See Other',
+			304 => 'Not Modified',
+			305 => 'Use Proxy',
+			306 => 'Switch Proxy',
+			307 => 'Temporary Redirect',
+			400 => 'Bad Request',
+			401 => 'Unauthorized',
+			402 => 'Payment Required',
+			403 => 'Forbidden',
+			404 => 'Not Found',
+			405 => 'Method Not Allowed',
+			406 => 'Not Acceptable',
+			407 => 'Proxy Authentication Required',
+			408 => 'Request Timeout',
+			409 => 'Conflict',
+			410 => 'Gone',
+			411 => 'Length Required',
+			412 => 'Precondition Failed',
+			413 => 'Request Entity Too Large',
+			414 => 'Request-URI Too Long',
+			415 => 'Unsupported Media Type',
+			416 => 'Requested Range Not Satisfiable',
+			417 => 'Expectation Failed',
+			418 => 'I\'m a teapot',
+			422 => 'Unprocessable Entity',
+			423 => 'Locked',
+			424 => 'Failed Dependency',
+			425 => 'Unordered Collection',
+			426 => 'Upgrade Required',
+			449 => 'Retry With',
+			450 => 'Blocked by Windows Parental Controls',
+			500 => 'Internal Server Error',
+			501 => 'Not Implemented',
+			502 => 'Bad Gateway',
+			503 => 'Service Unavailable',
+			504 => 'Gateway Timeout',
+			505 => 'HTTP Version Not Supported',
+			506 => 'Variant Also Negotiates',
+			507 => 'Insufficient Storage',
+			509 => 'Bandwidth Limit Exceeded',
+			510 => 'Not Extended'
+	);
+	
     public static function processCommand($url, $method="GET", $headerType="XML", $src="") {
 
         $method = strtoupper($method);
@@ -51,6 +110,20 @@ class Utils {
         if (preg_match("/^(https)/i", $url))
             curl_setopt($session, CURLOPT_SSL_VERIFYPEER, false);
         $result = curl_exec($session);
+        
+        $header  = curl_getinfo( $session );
+        
+        if($header['http_code'] != 200)
+        {
+        	throw new Exception("Error Code: " . $header['http_code'] . ", " .Utils::$http_codes[$header['http_code']]);
+        }
+        else {
+        	if(preg_match("/You have processed/i", $result) || preg_match("/Your pricing plan allows only/i", $result))
+        	{
+        		throw new Exception($result);
+        	}
+        }
+        
         curl_close($session);
 
         return $result;
